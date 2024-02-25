4d126db7369fe8bfe8a4c1e48d12c77456a4a26b
Alois <alois9866@gmail.com>
2024-02-25 06:39:30 +0300

    Disable bar by default

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 18;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ComicCode Nerd Font:size=8" };
 static const char dmenufont[]       = "ComicCode Nerd Font:size=8";
