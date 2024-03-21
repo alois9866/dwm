6909f1ccad1f7e419897d6faddfbaecf40992eca
Alois <alois9866@gmail.com>
2024-03-21 20:04:55 +0300

    Remove centered layout (tcl)

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 18;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;        /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ComicCode Nerd Font:size=8" };
@@ -49,7 +49,6 @@ static const Layout layouts[] = {
 	{ "[|]",      tile },    /* first entry is default */
 	{ "~~~",      NULL },    /* no layout function means floating behavior */
 	{ "O_O",      monocle },
-	{ "|O|",      tcl },
 };
 
 /* key definitions */
@@ -78,7 +77,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	/* { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, */
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
 	{ MODKEY,                       XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
